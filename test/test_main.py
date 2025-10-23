eed9b5fcc4f1efdb99d7f56757788537ce520554
Keith <katherine.zatsepilova@yandex.ru>
2025-10-23 13:59:24 +0300

    Fix test for updated get_path_from_arguments function

--- test/test_main.py
+++ test/test_main.py
@@ -4,8 +4,13 @@ import pytest
 
 
 @pytest.fixture()
-def correct_arguments_string() -> tuple[list[str], str]:
-    return ["-p", "/home/user/file.txt"], "/home/user/file.txt"
+def correct_arguments_string() -> tuple[list[str], tuple[str, str]]:
+    return ["-p", "/home/user/file.txt"], ("/home/user/file.txt", "text")
+
+
+@pytest.fixture()
+def correct_arguments_string_with_format() -> tuple[list[str], tuple[str, str]]:
+    return ["-p", "/home/user/file.txt", "-f", "xml"], ("/home/user/file.txt", "xml")
 
 
 @pytest.fixture()
@@ -14,10 +19,23 @@ def noncorrect_arguments_string() -> list[str]:
 
 
 def test_get_path_from_correct_arguments(
-    correct_arguments_string: tuple[list[str], str]
+    correct_arguments_string: tuple[list[str], tuple[str, str]]
 ) -> None:
-    path = get_path_from_arguments(correct_arguments_string[0])
-    assert path == correct_arguments_string[1]
+    path, file_format = get_path_from_arguments(correct_arguments_string[0])
+    expected_path, expected_format = correct_arguments_string[1]
+    assert path == expected_path
+    assert file_format == expected_format
+
+
+def test_get_path_from_correct_arguments_with_format(
+    correct_arguments_string_with_format: tuple[list[str], tuple[str, str]]
+) -> None:
+    path, file_format = get_path_from_arguments(
+        correct_arguments_string_with_format[0]
+    )
+    expected_path, expected_format = correct_arguments_string_with_format[1]
+    assert path == expected_path
+    assert file_format == expected_format
 
 
 def test_get_path_from_noncorrect_arguments(
@@ -26,3 +44,4 @@ def test_get_path_from_noncorrect_arguments(
     with pytest.raises(SystemExit) as e:
         get_path_from_arguments(noncorrect_arguments_string[0])
     assert e.type == SystemExit
+    
