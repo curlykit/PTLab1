6b361d5b0428c77054ed9d64d79b7cfddf4feac1
Keith <katherine.zatsepilova@yandex.ru>
2025-10-23 12:51:18 +0300

    Fix final PEP8 issues: newlines and line length

--- src/CalcRating.py
+++ src/CalcRating.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 from Types import DataType
+
 RatingType = dict[str, float]
+
+
 class CalcRating:
- 
+
     def __init__(self, data: DataType) -> None:
         self.data: DataType = data
         self.rating: RatingType = {}
+
     def calc(self) -> RatingType:
         for key in self.data:
             self.rating[key] = 0.0
@@ -13,3 +17,4 @@ class CalcRating:
                 self.rating[key] += subject[1]
             self.rating[key] /= len(self.data[key])
         return self.rating
+
