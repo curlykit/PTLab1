96741e9b8ab06bdd0a3512a0a8ec9e6ba7628770
Keith <katherine.zatsepilova@yandex.ru>
2025-10-23 13:57:38 +0300

    Remove all trailing whitespace completely

--- src/FindPerfectStudent.py
+++ src/FindPerfectStudent.py
@@ -9,12 +9,12 @@ class FindPerfectStudent:
 
     def find(self) -> str:
         perfect_students = []
-        
+
         for student_name, subjects in self.data.items():
             all_perfect = all(grade == 100 for _, grade in subjects)
             if all_perfect:
                 perfect_students.append(student_name)
-        
+
         if perfect_students:
             return perfect_students[0]
         else:
--- src/XMLDataReader.py
+++ src/XMLDataReader.py
@@ -9,21 +9,21 @@ class XMLDataReader(DataReader):
     def read(self, path: str) -> DataType:
         with open(path, 'r', encoding='utf-8') as file:
             xml_data = xmltodict.parse(file.read())
-        
+
         students: DataType = {}
-        
+
         if 'root' in xml_data:
             for student_element in xml_data['root']:
                 if isinstance(student_element, str):
                     continue
-                    
+
                 student_name = list(student_element.keys())[0]
                 subjects = []
-                
+
                 for subject, grade in student_element[student_name].items():
                     if subject != '@name':
                         subjects.append((subject, int(grade)))
-                
+
                 students[student_name] = subjects
-        
+
         return students
