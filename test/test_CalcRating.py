611004916d7fb0420573a3fdecf8bb8ba0fc697e
Keith <katherine.zatsepilova@yandex.ru>
2025-10-23 12:56:01 +0300

    Fix final line length issues

--- test/test_CalcRating.py
+++ test/test_CalcRating.py
@@ -40,4 +40,5 @@ class TestCalcRating:
         for student in rating.keys():
             rating_score = rating[student]
             expected_score = input_data[1][student]
-            assert pytest.approx(rating_score, abs=0.001) == expected_score
+            assert pytest.approx(rating_score, abs=0.001) == \
+                expected_score
