c2929e78cdb5274dcebc69b6528d5fb8b39616f3
Keith <katherine.zatsepilova@yandex.ru>
2025-10-23 13:29:57 +0300

    Fix TextDataReader class

--- src/TextDataReader.py
+++ src/TextDataReader.py
@@ -1,36 +1,22 @@
 # -*- coding: utf-8 -*-
-import pytest
 from src.Types import DataType
+from src.DataReader import DataReader
 
 
-class TestTextDataReader:
+class TextDataReader(DataReader):
 
-    @pytest.fixture()
-    def file_and_data_content(self) -> tuple[str, DataType]:
-        text = ("Иванов Константин Дмитриевич\n" +
-                "   математика:91\n" + "   химия:100\n" +
-                "Петров Петр Семенович\n" +
-                "   русский язык:87\n" + "   литература:78\n")
+    def __init__(self) -> None:
+        self.key: str = ""
+        self.students: DataType = {}
 
-        data = {
-            "Иванов Константин Дмитриевич": [
-                ("математика", 91), ("химия", 100)
-            ],
-            "Петров Петр Семенович": [
-                ("русский язык", 87), ("литература", 78)
-            ]
-        }
-        return text, data
-
-    @pytest.fixture()
-    def filepath_and_data(
-        self, file_and_data_content: tuple[str, DataType], tmpdir
-    ) -> tuple[str, DataType]:
-        dir_path = tmpdir.mkdir("datadir")
-        p = dir_path.join("my_data.txt")
-        p.write_text(file_and_data_content[0], encoding='utf-8')
-        return str(p), file_and_data_content[1]
-
-    def test_read(self, filepath_and_data: tuple[str, DataType]) -> None:
-        file_content = TextDataReader().read(filepath_and_data[0])
-        assert file_content == filepath_and_data[1]
+    def read(self, path: str) -> DataType:
+        with open(path, encoding='utf-8') as file:
+            for line in file:
+                if not line.startswith(" "):
+                    self.key = line.strip()
+                    self.students[self.key] = []
+                else:
+                    subj, score = line.split(":", maxsplit=1)
+                    self.students[self.key].append(
+                        (subj.strip(), int(score.strip())))
+        return self.students
