cb7144c6b626f0b3d10e54332e1649c9ae72b256
Keith <katherine.zatsepilova@yandex.ru>
2025-10-25 22:19:25 +0300

    Fix XMLDataReader parsing issues

--- src/XMLDataReader.py
+++ src/XMLDataReader.py
@@ -9,21 +9,28 @@ class XMLDataReader(DataReader):
     def read(self, path: str) -> DataType:
         with open(path, 'r', encoding='utf-8') as file:
             xml_data = xmltodict.parse(file.read())
-
+        
         students: DataType = {}
-
-        if 'root' in xml_data:
-            for student_element in xml_data['root']:
-                if isinstance(student_element, str):
-                    continue
-
+        
+        if 'root' in xml_data and xml_data['root'] is not None:
+            student_elements = xml_data['root']
+            
+            # Если только один студент, xmltodict возвращает dict вместо list
+            if isinstance(student_elements, dict):
+                student_elements = [student_elements]
+            
+            for student_element in student_elements:
+                # Получаем имя студента (первый ключ в словаре)
                 student_name = list(student_element.keys())[0]
+                student_data = student_element[student_name]
+                
                 subjects = []
-
-                for subject, grade in student_element[student_name].items():
-                    if subject != '@name':
+                
+                # Обрабатываем предметы и оценки
+                for subject, grade in student_data.items():
+                    if subject not in ['@name']:  # игнорируем атрибуты
                         subjects.append((subject, int(grade)))
-
+                
                 students[student_name] = subjects
-
+        
         return students
